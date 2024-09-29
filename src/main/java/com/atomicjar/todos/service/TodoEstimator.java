b680dc4fead1b0ea4a5c1a855e39c4afec5028a1
shelajev <shelajev@gmail.com>
2024-09-29 19:02:46 +0300

    improve the prompt a bit

--- src/main/java/com/atomicjar/todos/service/TodoEstimator.java
+++ src/main/java/com/atomicjar/todos/service/TodoEstimator.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @AiService
 public interface TodoEstimator {
 
-    @SystemMessage("You're a task estimator, I'm going to give you a title of a todo item, and you will return how long do you think it will take me to complete the said todo:")
+    @SystemMessage("You're a task estimator, I'm going to give you a title of a todo item, and you will return how long do you think it will take me to complete the said todo. Give answer in hours. Only respond with the estimate, no other text or explanation please. Todo:")
     String chat(String userMessage);
 }
 
